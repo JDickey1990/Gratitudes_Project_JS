ba6f521ed25b4e0e1dc9cb14671a7f1e448c28dc
jmd <jordan.michael.dickey@gmail.com>
2021-02-14 16:16:10 -0700

    Created a Gratitude class with a constructor function

--- Gratitudes_Project_JS-frontend/gratitude.js
+++ Gratitudes_Project_JS-frontend/gratitude.js
@@ -1,2 +1,15 @@
 //class for gratitude 
 // "class" static methods and "instance" (method on the prototype) methods for the post class
+class Gratitude {
+
+    static allGratitudes = []
+
+    constructor(gratitude){
+        this.id = gratitude.id
+        this.name = gratitude.name 
+        this.description = gratitude.description
+        this.reason = gratitude.reason
+        Gratitude.allGratitudes.push(this)
+    }
+
+}
