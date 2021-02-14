88fbd1d04068a18910e2aacbb4cc261394b13023
jmd <jordan.michael.dickey@gmail.com>
2021-02-14 16:27:07 -0700

    Created the ApiService class and built a consturctor method as well as the fetchGratitudes function

--- Gratitudes_Project_JS-frontend/service.js
+++ Gratitudes_Project_JS-frontend/service.js
@@ -1,1 +1,19 @@
 //all fetch functions
+class ApiService {
+
+    constructor(){
+        this.baseUrl = `http://localhost:3000`
+    }
+
+    fetchGratitudes(){
+        return fetch(`${this.baseUrl}/gratitudes`, {
+        headers: {
+            'content-type': 'application/json',
+            }
+        })
+        .then(res => res.json())
+    }      
+
+
+
+}
