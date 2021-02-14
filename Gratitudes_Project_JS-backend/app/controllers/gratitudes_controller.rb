d924f0c3414d7d0afad5426cbc90eaae69d93944
jmd <jordan.michael.dickey@gmail.com>
2021-02-14 15:53:05 -0700

    Adding a user and gratitude controller and resources for the controller routes

--- Gratitudes_Project_JS-backend/app/controllers/gratitudes_controller.rb
+++ Gratitudes_Project_JS-backend/app/controllers/gratitudes_controller.rb
@@ -1,2 +1,21 @@
 class GratitudesController < ApplicationController
+
+    def index 
+        gratitudes = Gratitude.all
+        render json: gratitudes
+    end 
+
+    def show 
+        gratitude = Gratitude.find(params[:id])
+        render json: gratitude
+    end 
+    
+     def create 
+
+     end 
+
+    def destroy 
+        gratitude = Gratitude.find(params[:id])
+        gratitude.destroy
+    end 
 end
--- Gratitudes_Project_JS-backend/app/controllers/users_controller.rb
+++ Gratitudes_Project_JS-backend/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    
+    def index 
+       
+    end 
+
+    def show 
+     
+    end 
+    
+    def create 
+       
+        
+    end 
+
+    def destroy 
+
+    end 
+    
 end
