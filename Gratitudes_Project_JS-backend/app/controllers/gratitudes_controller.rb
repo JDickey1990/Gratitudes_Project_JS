3da78659fcae777710898bd2e54e0a6359ef4521
jmd <jordan.michael.dickey@gmail.com>
2021-02-14 10:21:29 -0700

    Creating the User and Graitudes models, controllers and DB Migrations

---
+++ Gratitudes_Project_JS-backend/app/controllers/gratitudes_controller.rb
@@ -0,0 +1,2 @@
+class GratitudesController < ApplicationController
+end
---
+++ Gratitudes_Project_JS-backend/db/migrate/20210214171633_create_gratitudes.rb
@@ -0,0 +1,9 @@
+class CreateGratitudes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gratitudes do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
