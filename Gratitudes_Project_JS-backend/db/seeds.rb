487a5188d234861754d95d381bcc56ad5fe79911
jmd <jordan.michael.dickey@gmail.com>
2021-02-20 10:52:25 -0700

    Adding gratitude objects in the seed file

--- Gratitudes_Project_JS-backend/db/seeds.rb
+++ Gratitudes_Project_JS-backend/db/seeds.rb
@@ -9,9 +9,20 @@
 sarah = User.create(name: "Sarah")
 kenz = User.create(name: "Kenz")
 
+gratitudes = ["Money In The Bank", "Good Friends", "Freedom Of Religion", "Parents", "Weekends", "Pets", "Learning From Mistakes", "Opportunity To Get An Education", 
+"Having A Home", "The Ability To Read", "Breathing Fresh Air", "Laughter", "Sunshine", "Time", "Clean Water", "Books", "Kindness Of A Stranger","Campfires", "Art", 
+"Mountains", "Grocery Stores", "Sunsets", "Moon & Stars", "Air Conditioning", "Oceans", "Modern Medicine", "Music", "Modern Medicine", "Entrpreneurs", "Inventors"
+]
 
 #Create Gratitudes
-family =sarah.gratitudes.build(name: "Family", description: "My husband and my kids.", reason: "They keep me grounded")
-family.save
+
+gratitudes.each do |gratitude|
+    new_gratitude = sarah.gratitudes.build(name: gratitude)
+    new_gratitude.save
+end
+# family =sarah.gratitudes.build(name: "Family", description: "My husband and my kids.", reason: "They keep me grounded")
+# family.save
 home =kenz.gratitudes.build(name:"Home", description: "The house we live in currently", reason: "It keeps us warm and we have shelter so at night if it is raining or snowing we are safe.")
 home.save
+
+# goodhealth = sarah.gratitudes.build(name: "Good Health")
