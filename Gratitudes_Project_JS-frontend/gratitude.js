87943658a7dc3ad8f83f7026d5067158ddfb7ff8
jmd <jordan.michael.dickey@gmail.com>
2021-02-21 21:05:12 -0700

    setting the form field to an empty string after being submitted and creating eventlisteners for the delete buttons on the page

--- Gratitudes_Project_JS-frontend/gratitude.js
+++ Gratitudes_Project_JS-frontend/gratitude.js
@@ -40,14 +40,28 @@ class Gratitude {
 
     static eventListeners(userId){
         let newGratitude = document.querySelector(".new-gratitude-form")
+        let deleteButtons = document.querySelectorAll(".delete-gratitude")
+
         newGratitude.addEventListener("submit", (e) => {
             e.preventDefault()
             const name = e.target.name.value
             const description = e.target.description.value
             const reason = e.target.reason.value
             const image = e.target.image.value
-            Gratitude.createGratitude(name, description,reason, image, userId)     
+            Gratitude.createGratitude(name, description,reason, image, userId)    
+            e.target.name.value = ""
+            e.target.description.value = ""
+            e.target.reason.value = ""
+            e.target.image.value = ""
+        })
+
+        deleteButtons.forEach(button => {
+            button.addEventListener("click", (e) =>{
+                e.preventDefault()
+                apiService.fetchDeleteGratitude(e.target.dataset.gratitudeId)
+                e.target.parentElement.remove()
             })
+        })
     }
 
     static createGratitude(name, description,reason, image, userId){
@@ -55,36 +69,42 @@ class Gratitude {
             .then(gratitudeData => 
                  {
                 const newGratitude = new Gratitude(gratitudeData)
-               Gratitude.renderNewGratitude(newGratitude)
-        }) 
+                Gratitude.renderNewGratitude(newGratitude)
+            }) 
         
     }
 
     static renderNewGratitude(gratitude){
         const list = document.querySelector(".gratitudes-container")
-              
-                const div = document.createElement("div")
-                const h2 = document.createElement("h2")
-                const h3 = document.createElement("h3")
-                const h4 = document.createElement("h4")
-                const p = document.createElement("p")
-                const img = document.createElement("img")
-                
-                div.setAttribute("class", "card")
-                div.setAttribute("data-id", gratitude.id)
-                img.setAttribute("src",gratitude.image)
-                img.setAttribute("class", "card-img" )
-                
-                h3.innerHTML = gratitude.name
-                h4.innerHTML = `Reason: ${gratitude.reason}`
-                p.innerHTML = `Description: ${gratitude.description}`
-             
-                div.appendChild(h3)
-                div.appendChild(h4)
-                div.appendChild(p)
-                div.appendChild(img)
-            
-                list.appendChild(div)
+        const div = document.createElement("div")
+        const h2 = document.createElement("h2")
+        const h3 = document.createElement("h3")
+        const h4 = document.createElement("h4")
+        const p = document.createElement("p")
+        const img = document.createElement("img")
+        const button = document.createElement("button")
+        
+        div.setAttribute("class", "card")
+        div.setAttribute("data-id", gratitude.id)
+        img.setAttribute("src",gratitude.image)
+        img.setAttribute("class", "card-img" )
+        button.setAttribute("class", "delete-gratitude")
+        button.setAttribute("data-gratitude-id", gratitude.id)
+
+        h3.innerHTML = gratitude.name
+        h4.innerHTML = `Reason: ${gratitude.reason}`
+        p.innerHTML = `Description: ${gratitude.description}`
+        button.innerHTML = "Delete"
+        
+        div.appendChild(h3)
+        div.appendChild(h4)
+        div.appendChild(p)
+        div.appendChild(img)
+        div.appendChild(button)
+    
+        list.appendChild(div)
+
+        Gratitude.eventListeners()
     }
 
     
