e387a7cdce18e0686d247028ca5c29d33ac92284
jmd <jordan.michael.dickey@gmail.com>
2021-02-24 19:44:30 -0700

    refactoring the renderUserGratitudes function to invoke the Ui render gratitude function and generate the html for the gratitude objects

--- Gratitudes_Project_JS-frontend/src/user.js
+++ Gratitudes_Project_JS-frontend/src/user.js
@@ -18,7 +18,6 @@ class User {
         })
     }
     
-    
     static generateUser(userName){
         apiService.fetchUser(userName)
             .then(userData => 
@@ -26,44 +25,16 @@ class User {
                 const newUser = new User(userData)
                 Ui.renderUsersPage()
                 newUser.renderUserGratitudes()
+                Gratitude.eventListeners(newUser.id)
         }) 
         
     }
 
-
     renderUserGratitudes(){
         const list = document.querySelector(".gratitudes-container")
             this.gratitudes.forEach(gratitude =>{
-            //   call render gratitude here
-                const div = document.createElement("div")
-                const h2 = document.createElement("h2")
-                const h3 = document.createElement("h3")
-                const h4 = document.createElement("h4")
-                const p = document.createElement("p")
-                const img = document.createElement("img")
-                const button = document.createElement("button")
-                
-                div.setAttribute("class", "card")
-                div.setAttribute("data-id", gratitude.id)
-                img.setAttribute("src",gratitude.image)
-                img.setAttribute("class", "card-img" )
-                button.setAttribute("class", "delete-gratitude")
-                button.setAttribute("data-gratitude-id", gratitude.id)
-
-                h3.innerHTML = gratitude.name
-                h4.innerHTML = `Reason: ${gratitude.reason}`
-                p.innerHTML = `Description: ${gratitude.description}`
-                button.innerHTML = "Delete"
-                
-                div.appendChild(h3)
-                div.appendChild(h4)
-                div.appendChild(p)
-                div.appendChild(img)
-                div.appendChild(button)
-            
-                list.appendChild(div)
-            })  
-        Gratitude.eventListeners(this.id)
+                Ui.renderGratitude(gratitude)
+            })
     }
 
 }
